946045645af08b7aa08614bdbae44ab1e81e1b30
unknown <asadbekliverpool@mail.ru>
2023-04-08 15:33:44 +0500

    com

---
+++ lib/pages/detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailPage  extends StatefulWidget {
+  static final String id = "detail_page";
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _State();
+}
+
+class _State extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
